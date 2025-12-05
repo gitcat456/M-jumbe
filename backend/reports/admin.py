13d1a813a1ac3777534dfb2c25606cb7056369dd
gitcat456 <okothdenzel65@gmail.com>
2025-12-05 12:52:56 +0300

    registers the models to the admin site

--- backend/reports/admin.py
+++ backend/reports/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
-# Register your models here.
+from django.contrib import admin
+from .models import Category, Report
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name']
+    search_fields = ['name']
+
+@admin.register(Report)
+class ReportAdmin(admin.ModelAdmin):
+    list_display = ['id', 'title', 'category', 'status', 'created_at']
+    list_filter = ['category', 'status', 'created_at']
+    search_fields = ['title', 'location', 'description']
+    list_per_page = 20
