02ebea1abc7f61c9cb239d25469c187ffa02eaa3
brianwaw <bwaweru408@gmail.com>
2025-12-05 13:01:40 +0100

    admin handling

--- backend/reports/views.py
+++ backend/reports/views.py
@@ -1,13 +1,17 @@
-from django.shortcuts import render
 from rest_framework import viewsets
 from .models import Category, Report
 from .serializers import CategorySerializer, ReportSerializer
 
+
 class CategoryViewSet(viewsets.ModelViewSet):
+    authentication_classes = []
+    permission_classes = []
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
 
+
 class ReportViewSet(viewsets.ModelViewSet):
+    authentication_classes = []
+    permission_classes = []
     queryset = Report.objects.all()
     serializer_class = ReportSerializer
-
