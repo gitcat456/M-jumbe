cdf0c358572a1adba10e1832658f6fec97f79b53
gitcat456 <okothdenzel65@gmail.com>
2025-12-05 12:50:10 +0300

    creates reports and categories models

--- backend/reports/models.py
+++ backend/reports/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
-# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    
+    def __str__(self):
+        return self.name 
+    
+class Report(models.Model):
+    
+    STATUS_CHOICES = (  
+        ('pending', 'Pending'),
+        ('in_progress', 'In Progress'),  
+        ('completed', 'Completed')
+    )
+    
+    title = models.CharField(max_length=100)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='reports')
+    description = models.TextField()
+    location = models.CharField(max_length=200) 
+    photos = models.ImageField(upload_to='report_pics/')
+    status = models.CharField(
+        max_length=20,  
+        choices=STATUS_CHOICES, 
+        default='pending'
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return self.title 
