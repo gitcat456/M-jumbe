2c06d63d902e0a400380486523ac3a4c6634fdba
gitcat456 <okothdenzel65@gmail.com>
2025-12-05 12:54:57 +0300

    view classes for the models

--- backend/reports/views.py
+++ backend/reports/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Category, Report
+from .serializers import CategorySerializer, ReportSerializer
 
-# Create your views here.
+class CategoryViewSet(viewsets.ModelViewSet):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+
+class ReportViewSet(viewsets.ModelViewSet):
+    queryset = Report.objects.all()
+    serializer_class = ReportSerializer
+
